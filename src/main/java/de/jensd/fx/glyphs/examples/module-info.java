7e848a4e3bcf8b1121f80a811c494e8a1fca1c42
Jens Deters <jens.deters@codecentric.de>
2017-09-26 10:23:16 +0200

    working on Java 9 modules support

---
+++ src/main/java/de/jensd/fx/glyphs/examples/module-info.java
@@ -0,0 +1,15 @@
+module de.jensd.fx.glyphs.examples {
+    exports de.jensd.fx.glyphs.examples;
+    requires de.jensd.fx.glyphs.commons;
+    requires de.jensd.fx.glyphs.controls;
+    requires de.jensd.fx.glyphs.weathericons;
+    requires de.jensd.fx.glyphs.iconsfivetwofive;
+    requires de.jensd.fx.glyphs.fontawesome;
+    requires de.jensd.fx.glyphs.materialdesignicons;
+    requires de.jensd.fx.glyphs.materialicons;
+    requires de.jensd.fx.glyphs.octicons;
+    requires javafx.graphics;
+    requires java.logging;
+    requires javafx.fxml;
+    requires javafx.controls;
+}
